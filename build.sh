33ed2620d2e7145b046be66da48992219f87d60f
Virgillio Gonzenbach <vjgonzenbach@gmail.com>
2024-06-19 22:13:52 +0000

    automate inputs

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
-PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+#!/bin/bash
 
-terraform init && terraform apply -var="project_id=$PROJECT_ID"
+# Must set project id with `gcloud config set project <YOUR_PROJECT>`
+PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+echo "project_id = \"""$PROJECT_ID""\"" > terraform.auto.tfvars
+
+terraform init
+
+if [ "$1" = "plan" ]; then
+    terraform plan 
+
+elif [ "$1" = "apply" ]; then
+    terraform apply 
+
+else 
+    echo "Choose arg 'plan' or 'apply'"
+fi
