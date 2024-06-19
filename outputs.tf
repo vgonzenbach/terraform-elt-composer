a122d0fc3b553e048d9b4b168a3fe7b96eb6a6c6
Virgillio Gonzenbach <vjgonzenbach@gmail.com>
2024-06-19 22:13:29 +0000

    add outputs

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "composer_env_name" {
+  description = "Name of the Composer environment"
+  value       = var.composer_env_name
+}
+
+output "composer_service_account" {
+    description = "Email of the Composer environment service account"
+    value = module.service_accounts.email
+    sensitive = true
+}
+
+output "region" {
+    description = "Region set by user (used by Composer)"
+    value = var.region
+}
